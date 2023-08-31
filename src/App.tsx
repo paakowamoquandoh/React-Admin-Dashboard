b66ed3fda360664a21d2ce0fc0a4585b44e61153
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-31 19:34:10 +0000

    tailwind config update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 // import React from 'react';
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-     <button className="bg-cyan-500 hover:bg-cyan-600">
-      Save changes
-     </button>
+      <button className="bg-cyan-500 hover:bg-cyan-600 p-5">
+        Save changes
+      </button>
+      <div className="font-sans text-primary bg-gradient-to-br from-primary to-white text-3xl">
+        Custom Styles!
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
-  // other configurations...
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans'],
+      },
+      colors: {
+        primary: '#FF5733',
+      },
+      gradientExample: {
+        'gradient-to-br': 'linear-gradient(to bottom right, #FF5733, #FFCC33)', // Your gradient colors
+      },
+      fontSize: {
+        '3xl': '1.875rem', // Customize with your desired font size
+      },
+    },
+  },
+  plugins: [],
 }
+
+
